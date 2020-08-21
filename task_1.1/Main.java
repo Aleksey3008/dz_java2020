a64ea23814782d967a31d6523faba42a1811ae79
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-21 12:08:19 +0300

    Create Main.java

---
+++ task_1.1/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Aleksey";
+        int age = 51;
+        String city = "Moscow";
+        System.out.println("Привет " +name+" " +age+ " "+city);
+    }
+}
