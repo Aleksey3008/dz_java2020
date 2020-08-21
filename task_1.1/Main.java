f421476556f2ee8b32f17c1450c5db90683aafa7
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-21 14:03:25 +0300

    Delete Main.java

--- task_1.1/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-        String name = "Aleksey";
-        int age = 51;
-        String city = "Moscow";
-        System.out.println("Привет " +name+" " +age+ " "+city);
-    }
-}
